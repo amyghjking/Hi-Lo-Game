0c16e8dbe79d2fb6a48180f74f972d6d3336c58c
Amy King <amy.king@optimalworkshop.com>
2023-11-14 11:14:57 +1300

    Syntax edits to printing number

--- hilo.rb
+++ hilo.rb
@@ -3,5 +3,5 @@ puts "Welcome to the Hi-Lo game."
 puts "Guess a random number from 1 to 100 (inclusive)."
 puts "You have 6 guesses."
 
-$random_num = rand(1..100)
-puts "Random number: #$random_num"
+random_num = rand(1..100)
+puts "Random number: #{random_num}"
