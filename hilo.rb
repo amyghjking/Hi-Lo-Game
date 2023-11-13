e15e5507a8cb353f7e8393d3484f9f44beded6db
Amy King <amy.king@optimalworkshop.com>
2023-11-14 11:45:03 +1300

    Accepts user input and compares to generated number

--- hilo.rb
+++ hilo.rb
@@ -5,3 +5,14 @@ puts "You have 6 guesses."
 
 random_num = rand(1..100)
 puts "Random number: #{random_num}"
+
+guess = gets.to_i
+
+# comparing randomly generated number with user guess
+if random_num == guess
+    puts "You guessed correctly"
+elsif guess < random_num
+    puts "Your guess was too low!"
+elsif guess > random_num
+    puts "Your guess was too high!"
+end
