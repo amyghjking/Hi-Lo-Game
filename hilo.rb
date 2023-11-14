45ab1cf5070e2434a4f6581dc2d145ba2e7b410a
Amy King <amy.king@optimalworkshop.com>
2023-11-14 15:07:24 +1300

    Only allows integer input from user

--- hilo.rb
+++ hilo.rb
@@ -4,9 +4,14 @@ puts "Guess a random number from 1 to 100 (inclusive)."
 puts "You have 6 guesses."
 
 random_num = rand(1..100)
-puts "Random number: #{random_num}"
 
-guess = gets.to_i
+# only allow integer input
+guess = gets
+while guess.strip != guess.to_i.to_s  
+    puts "Guess a number!"
+    guess = gets
+end
+guess = guess.to_i
 
 # comparing randomly generated number with user guess
 if random_num == guess
