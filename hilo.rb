1b53e4f6b722b6fe95780aeb592c7bcfc1a5940f
Amy King <amy.king@optimalworkshop.com>
2023-11-14 15:52:33 +1300

    Asks the user to guess a number

--- hilo.rb
+++ hilo.rb
@@ -4,6 +4,7 @@ puts "Guess a random number from 1 to 100 (inclusive)."
 puts "You have 6 guesses."
 
 random_num = rand(1..100)
+puts "Please guess a number."
 
 # only allow integer input
 guess = gets
