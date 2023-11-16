b178b3bdc3c3985ce63ac1a619013981c8bee1a6
Amy King <amy.king@optimalworkshop.com>
2023-11-16 14:03:33 +1300

    Documenting comments added

--- hilo.rb
+++ hilo.rb
@@ -1,12 +1,15 @@
 #!/usr/bin/env ruby
 
+# Game to guess random number
 class HiLoGame    
     @@already_guessed = Array.new
    
-     def initialize(max_guesses)
+    # Constructor to initialise the class with a maximum number of guesses
+    def initialize(max_guesses)
 	@max_guesses = max_guesses
     end
 
+    # Welcome message for the game
     def welcome
 	puts "Welcome to the Hi-Lo game.\nGuess a random whole number from 1 to 100 (inclusive)."
 	puts "You will have a total of 6 guesses.\nYou cannot guess the same number more than once."
@@ -17,6 +20,7 @@ class HiLoGame
 	random_num = rand(1..100)
     end
 
+    # Allows user to guess number and validates input by calling validate_guess
     def guess_a_number(attempt_number, random_num)
         guesses_left = @max_guesses - attempt_number
 	puts "You have #{guesses_left} guesses left."
@@ -29,6 +33,7 @@ class HiLoGame
 	check_guess(guess, random_num)
     end
  
+    # Checks user input is a whole number between the bounds and hasn't already been guessed
     def validate_guess(guess)
 	while guess.strip != guess.to_i.to_s || guess.to_i < 1 || guess.to_i > 100 || @@already_guessed.include?(guess.to_i)
 	    puts "Guess a whole number from 1 to 100 (inclusive)!\nYou cannot guess the same number more than once."
@@ -37,6 +42,7 @@ class HiLoGame
 	guess = guess.to_i
     end
 
+    # Checks if the guess is correct
     def check_guess(guess, random_num)
 	if guess == random_num
 	    puts "\nYou guessed correctly and won the game!"
@@ -48,18 +54,19 @@ class HiLoGame
 	end
     end
 
+    # Informs user they have lost
     def lost_game(random_num)
 	puts "\nYou made #{@max_guesses} incorrect guesses and lost the game."
 	puts "The random number was #{random_num}"
     end
 
-    ##
     # Getter
     def max_guesses
 	@max_guesses
     end
 end
 
+# main starts here
 game = HiLoGame.new(6)
 game.welcome
 random_num = game.get_random_num
